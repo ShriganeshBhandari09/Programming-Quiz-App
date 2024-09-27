36590b34e47c36825c9d78726c3631ccb56f6848
ShriganeshBhandari09 <shriganesh.bhandari693@gmail.com>
2024-09-27 21:56:03 +0530

    Updated Quiz Questions Page

--- js/script.js
+++ js/script.js
@@ -150,71 +150,104 @@ var questions = [
   },
 ];
 
-var questionId = 0;
-var totalQuestions = document.getElementById("totalQuestions");
-var currentQuestion = document.getElementById("currentQuestion");
+var marks = 0;
 var question = document.getElementById("question");
+var currntQuestion = document.getElementById("currentQuestion");
+var totalQuestions = document.getElementById("totalQuestions");
+var questionheading = document.getElementById("question-heading");
+var mainContainer = document.getElementById("main-container");
+
 var optionOne = document.getElementById("option-one");
 var optionTwo = document.getElementById("option-two");
 var optionThree = document.getElementById("option-three");
-var optionFour = document.getElementById("option-four");
-var numberOfQuestions = document.getElementById("number of question");
-var backwardBtn = document.getElementById("backward-btn");
+var optionfour = document.getElementById("option-four");
 
+var labelOne = document.getElementById("label-one");
+var labelTwo = document.getElementById("label-two");
+var labelThree = document.getElementById("label-three");
+var labelFour = document.getElementById("label-four");
+
+var forwardBtn = document.getElementById("forward-btn");
+var submitBtn = document.getElementById("final-btn");
+var marksHeading = document.getElementById("marks");
+
+var questionId = 0;
 totalQuestions.innerText = questions.length;
+function generateQuestions() {
+  if (questionId === questions.length - 1) {
+    questionheading.innerText = "Last Question";
+    forwardBtn.style.display = "none";
+    submitBtn.style.display = "inline";
+  } else {
+    currntQuestion.innerText = questionId + 1;
+  }
+  question.innerText = `${questionId + 1}.${questions[questionId].question}`;
+  optionOne.value = questions[questionId].options[0];
+  optionTwo.value = questions[questionId].options[1];
+  optionThree.value = questions[questionId].options[2];
+  optionfour.value = questions[questionId].options[3];
+
+  labelOne.innerText = questions[questionId].options[0];
+  labelTwo.innerText = questions[questionId].options[1];
+  labelThree.innerText = questions[questionId].options[2];
+  labelFour.innerText = questions[questionId].options[3];
+  questionId++;
+  console.log("question id", questionId);
+}
+
+var answer = document.getElementById("answer");
+selectedQuestionid = 1;
+var selectedOptionsArray = [];
+function compareAns() {
+  var selectedOption = document.querySelector('input[name="options"]:checked');
+  if (selectedQuestionid === questions.length) {
+    selectedOptionsArray.push(selectedOption.value);
+    selectedOption.checked = false;
+    console.log(selectedOptionsArray);
+  } else {
+    if (!selectedOption) {
+      alert("please select the answer");
+    } else {
+      selectedOptionsArray.push(selectedOption.value);
+      selectedOption.checked = false;
+      console.log(selectedOptionsArray);
+      generateQuestions();
+      increaseSliderValue();
+      selectedQuestionid++;
+    }
+  }
+  console.log("The question id is", questionId);
+  console.log("The answer id is", selectedQuestionid);
+  console.log(" The array is", selectedOptionsArray);
+}
+
+// console.log(selectedOptionsArray);
+
+function calculateMarks() {
+  compareAns();
+  mainContainer.style.display = "none";
+  for (let index = 0; index < selectedOptionsArray.length; index++) {
+    var correctAnswer = questions[index].answer;
+    if (selectedOptionsArray[index] === correctAnswer) {
+      marks++;
+      console.log("Running");
+      console.log(marks);
+      marksHeading.innerText = `You scored ${marks} out of ${questions.length}`;
+    }
+  }
+}
 
 var slider = document.getElementById("rangeSliderValue");
 var totalQuestions = questions.length;
 var maxSliderValue = 100;
-var i = 1;
 var sliderValue = maxSliderValue / totalQuestions;
 console.log(sliderValue);
-slider.style.width = `${sliderValue}%`;
 function increaseSliderValue() {
   sliderValue = sliderValue + maxSliderValue / totalQuestions;
-  // console.log(sliderValue);
   if (sliderValue > 100) {
     sliderValue = 100;
     alert("Maximum Value Reached");
   }
   slider.style.width = `${sliderValue}%`;
   console.log(slider.style.width);
-  // console.log(slider);
-  generateQuestions();
-  // i++;
-  // console.log(`No of times clicked ${i}`);
-}
-
-function decreaseSliderValue() {
-  sliderValue = sliderValue - maxSliderValue / totalQuestions;
-  // console.log(sliderValue);
-  if (sliderValue < 0) {
-    sliderValue = 0;
-    alert("Maximum Value Reached");
-  }
-  slider.style.width = `${sliderValue}%`;
-  console.log(slider.style.width);
-}
-
-function generateQuestions() {
-  if (questionId === questions.length - 2) {
-    numberOfQuestions.innerText = "Last Second Question";
-  } else if (questionId === questions.length - 1) {
-    numberOfQuestions.innerText = "Last Question";
-  } else if (questionId === questions.length) {
-    alert("Last Question");
-    return;
-  } else {
-    currentQuestion.innerText = questionId + 1;
-  }
-  if (questionId >= 1) {
-    backwardBtn.style.visibility = "initial";
-  }
-  question.innerText = questions[questionId].question;
-  optionOne.innerText = questions[questionId].options[0];
-  optionTwo.innerText = questions[questionId].options[1];
-  optionThree.innerText = questions[questionId].options[2];
-  optionFour.innerText = questions[questionId].options[3];
-
-  questionId++;
 }
